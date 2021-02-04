d2bc0f9128a9a77b34d2cb2110b07373fde5ae80
Kyle <kjfossman@gmail.com>
2021-02-04 10:56:38 -0900

    commit tree 5a2b785c1e4a0f319c17a1a09a0a7dc1604e4801

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
+require 'pry'
+
 # Add  code here!
+def prime?(int)
+    return false if int <= 1
+    return true if int == 2 
+    array = (2..(int-1)).to_a
+   
+     array.each do |x|
+        if int % x == 0
+          return false
+        end
+    end
+    return true
+ 
+end
+
+
+
+
